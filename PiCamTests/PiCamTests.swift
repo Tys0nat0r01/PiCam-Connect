b57d7f0da2be6f2883f515a4ad9ad0fcf7e50a09
Miles Co Media <tysonjmiles@icloud.com>
2025-02-11 21:46:57 +1000

    Initial Commit

---
+++ PiCamTests/PiCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiCamTests.swift
+//  PiCamTests
+//
+//  Created by Tyson Miles on 11/2/2025.
+//
+
+import Testing
+@testable import PiCam
+
+struct PiCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
